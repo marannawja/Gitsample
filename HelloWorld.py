b02716d5ed677c7ce7c32e186d9b6d6e245aada6
marannawja <marannawja@gmail.com>
2019-12-15 05:35:55 +0630

    update git

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,8 @@
-print "Hello World"
+print ("Hello World")
 
-GitHub Test
+#1 2 3 4 5
+
+#this is the first comment
+
+spam = 1 # and this is the second comment
+text = "# This is not a comment because it's inside quotes"
