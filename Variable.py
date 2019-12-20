12b459595f16e916a32dc13f1738050f79d9659e
marannawja <marannawja@gmail.com>
2019-12-21 05:40:31 +0630

    Variable

---
+++ Variable.py
@@ -0,0 +1,115 @@
+#21.12.2019
+>>> spam = 1
+>>> spam
+1
+>>> text = "this is a string"
+>>> text
+'this is a string'
+>>> 50 - 5 * 6
+20
+>>> (50 - 5 * 6) / 4
+5.0
+>>> round(5.0)
+5
+>>> round(12.1234,2)
+12.12
+>>> 17 / 3
+5.666666666666667
+>>> 17 // 3
+5
+>>> 2 + 2
+4
+>>> 8 / 5
+1.6
+>>> 17 % 3
+2
+>>> 17 * 4 % 3
+2
+
+a = 1
+a (varible) = (assign) 1 (value)
+
+sale = 30000
+tax = 5 / 100
+total_tax = sale * tax
+total_tax
+total_price = sale + total_tax
+total_price
+
+print()
+
+3 * 'A' -- AAA
+2 * 'BC' + 'DE' -- BCBCDE
+10 * 'GH' + 3 -- error
+>>> 'gh' + '3'
+'gh3'
+>>> 'Py''thon'
+'Python'
+>>> text = ('Put several stringd within parentheses'
+...     'to have them joined together.')
+>>> text
+'Put several stringd within parenthesesto have them joined together.'
+>>> word = 'Programming'
+>>> word[0]
+'P'
+>>> word[5]
+'a'
+>>> word[-1]
+'g'
+>>> word[-2]
+'n'
+>>> word[-6]
+'a'
+>>> word[8]
+'i'
+>>> word[-2]
+'n'
+>>> word[-9]
+'o'
+>>> word[3:5]
+'gr'
+>>> word[4:9]
+'rammi'
+>>> word[:5]
+'Progr'
+>>> word[8:]
+'ing'
+>>> word[-2:5]
+''
+>>> word[5:-3]
+'amm'
+>>> word[:2] + word[3:]
+'Prgramming'
+>>>
+>>> print("""\
+... Usage: thingy
+...             -a
+...             -b
+...             -c
+... """)
+Usage: thingy
+                -a
+                -b
+                -c
+
+>>> 3 * 'un' + 'ium'
+'unununium'
+>>> 3 * 'A'
+'AAA'
+>>> 2 * 'BC' + 'DE'
+'BCBCDE'
+>>> 10 * 'GH' +3
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+TypeError: can only concatenate str (not "int") to str
+>>> 'gh
+  File "<stdin>", line 1
+    'gh
+      ^
+SyntaxError: EOL while scanning string literal
+>>> 'gh' + 3
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+TypeError: can only concatenate str (not "int") to str
+>>> 'gh' + '3"
+  File "<stdin>", line 1
