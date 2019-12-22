2c59fa1fed4ab38598361c362250e27c48c3c500
marannawja <marannawja@gmail.com>
2019-12-22 20:02:52 +0630

    List & Binary Code

---
+++ List.py
@@ -0,0 +1,77 @@
+#List 22.12.2019
+
+word = 'Programming'
+>>> word[0]
+'P'
+>>> word[5]
+'a'
+>>> word[-1]
+'g'
+>>> word[-2]
+'n'
+>>> word[-6]
+'a'
+>>> word[8]
+'i'
+>>> word[-2]
+'n'
+>>> word[-9]
+'o'
+>>> word[3:5]
+'gr'
+>>> word[4:9]
+'rammi'
+>>> word[:5]
+'Progr'
+>>> word[8:]
+'ing'
+>>> word[-2:5]
+''
+>>> word[5:-3]
+'amm'
+>>> word[:2] + word[3:]
+
+len(word) # Len = Length
+
+square = 'Square'
+len(square)
+len(word) + len(square)
+
+cube = [10, 20, 30, 45, 50]
+cube
+cube[3] = 40 #replace
+cube
+cube[5] = 60 #error out of range
+cube.append(60)
+cube.append(4+10+20+36)
+cube
+
+>>> programA = ['A','B','C','D','E']
+>>> programB = ['a','b','c','d','e']
+>>> programC = programA + programB
+ProgramD = [1, 2, 3, 4, 5]
+
+programC = programC + ProgramD
+programC[9:] = []
+programC[5:9] = ProgramD
+programC
+
+len(ProgramC)
+a = [10, 20, 30, 40, 50]
+b = [60, 70, 80, 90, 100]
+c = [110, 120, 130, 140, 150]
+x = [a, b, c]
+x
+
+x[0][]
+x[][3]
+x[1][2]
+x[][]
+x[0][0]
+x[1][1]
+x[]
+
+x[1][2] + x[2][0]
+x[0][1] - x[2][1]
+
+
